75774a1583d88b32766ad2411d250ff6bb3a9517
Rashin Arab <rashin@buddybuild.com>
2015-10-06 16:03:49 -0700

    Fixing build

---
+++ DerivedData/Build/Intermediates/VenmoDryDock.build/Debug-iphoneos/VenmoDryDock.build/Script-FE34DC2EDB9D41718C7B0D87.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-VenmoDryDock/Pods-VenmoDryDock-resources.sh"
+
--- VenmoDryDock/VDDAppDelegate.m
+++ VenmoDryDock/VDDAppDelegate.m
@@ -21,7 +21,6 @@
 // THE SOFTWARE.
 
 #import "VDDAppDelegate.h"
-#import <VENVersionTracker/VENVersionTracker.h>
 #import "VDDConstants.h"
 
 @implementation VDDAppDelegate
@@ -30,36 +29,11 @@
     [Parse setApplicationId:VDDParseAppId
                   clientKey:VDDParseClientKey];
     
-    [self startTrackingVersion];
     
     return YES;
 }
 
 
-- (void)startTrackingVersion {
-    // For details on using VENVersionTracker see -- https://github.com/venmo/VENVersionTracker
-    /*
-     The URL http://YOUR_BASE_URL/track/YOUR_CHANNEL_NAME should return the following JSON:
 
-     {
-        "version":{
-            "number":"0.1.0",
-            "mandatory":false,
-            "install_url":"<<ITMS INSTALL URL>>"
-        },
-        "min-version-number":0.1.0
-     }
-     */
-    [VENVersionTracker beginTrackingVersionForChannel:VDDChannelName
-                                       serviceBaseUrl:VDDBaseUrl
-                                         timeInterval:1800
-                                          withHandler:^(VENVersionTrackerState state, VENVersion *version) {
-                                              dispatch_sync(dispatch_get_main_queue(), ^{
-                                                  if (state == VENVersionTrackerStateDeprecated || state == VENVersionTrackerStateOutdated) {
-                                                      [version install];
-                                                  }
-                                              });
-                                          }];
-}
 
 @end
